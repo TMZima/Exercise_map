fcd752bdd3b0f7a929e2cd56a3045b420826b642
Trey Zima <trey.m.zima@gmail.com>
2024-11-22 15:43:48 -0700

    Added comments and logged the user details

--- map.js
+++ map.js
@@ -5,9 +5,16 @@ const users = [
   { firstName: "Charlie", lastName: "Brown", points: 180 },
 ];
 
+// Solution
+
+// Using map() method to create a new array of objects with fullName and membershipStatus properties
 const userDetails = users.map((user) => {
   return {
     fullName: `${user.firstName} ${user.lastName}`,
+    // If points are greater than 100, membershipStatus is "Premium", otherwise "Standard"
     membershipStatus: user.points > 100 ? "Premium" : "Standard",
   };
 });
+
+// Output
+console.log(userDetails);
