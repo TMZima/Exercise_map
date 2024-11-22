9b7fe86f8df4252e00b0c7e2edfc3f446ca38adc
Trey Zima <trey.m.zima@gmail.com>
2024-11-22 15:30:59 -0700

    Completed Exercise

--- map.js
+++ map.js
@@ -4,3 +4,10 @@ const users = [
   { firstName: "Bob", lastName: "Smith", points: 99 },
   { firstName: "Charlie", lastName: "Brown", points: 180 },
 ];
+
+const userDetails = users.map((user) => {
+  return {
+    fullName: `${user.firstName} ${user.lastName}`,
+    membershipStatus: user.points > 100 ? "Premium" : "Standard",
+  };
+});
