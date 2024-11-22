dfb42b38901468877d2a900c5a0fbdd63d725e92
Trey Zima <trey.m.zima@gmail.com>
2024-11-22 15:16:13 -0700

    Updated README and added starter code

---
+++ map.js
@@ -0,0 +1,6 @@
+// Starter code:
+const users = [
+  { firstName: "Alice", lastName: "Johnson", points: 120 },
+  { firstName: "Bob", lastName: "Smith", points: 99 },
+  { firstName: "Charlie", lastName: "Brown", points: 180 },
+];
